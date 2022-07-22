e2384facb33e7c346feb4b9d595f0abcd39ce3d8
Erlan <erlankambarov94@gmail.com>
2022-07-23 01:33:49 +0600

    Erlan: Закончил Header и адаптировал

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import cls from './Header.module.scss'
+import logo from '../../assets/img/logo.png'
+import { list } from './List';
+import { Link } from 'react-router-dom';
+import { VscListSelection } from 'react-icons/vsc'
+
+const Header = () => {
+  
+  return (
+    <React.Fragment>
+      <nav className={cls.nav}>
+        <div className={cls.logo}>
+          <img src={logo} alt="" />
+        </div>
+        <div className={cls.list}>
+          {
+            list.map(({ id, title, route }) => (
+              <ul className={cls.list} key={id}>
+                <li key={id}>
+                  <Link to={route}>{title}</Link>
+                </li>
+              </ul>
+            ))
+          }
+        </div>
+        <div className={cls.burger_menu}>
+          <p><VscListSelection /></p>
+        </div>
+      </nav>
+    </React.Fragment >
+  )
+}
+
+export default Header
