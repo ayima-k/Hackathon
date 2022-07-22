7b287e17a78666ffab18be9b7de59e8458f16333
altynovbayel <altynovich30@gmail.com>
2022-07-23 01:49:43 +0600

    bayel: profile page

--- src/pages/General/pages/Profile/Profile.jsx
+++ src/pages/General/pages/Profile/Profile.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import {Typography} from "@mui/material";
+import c from './Profile.module.scss'
+import {BiUserCircle} from 'react-icons/bi'
 
 function Profile() {
   return (
-    <div>
-      Profile
-    </div>
+    <React.Fragment>
+      <Typography variant={'h5'} align={'center'} >
+        Ваш профиль
+      </Typography>
+      <div className={c.container}>
+        <div className={c.userPhoto}>
+          <img src="https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png" alt="" />
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
