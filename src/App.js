3e26a44c5237989c66bd425bc1fcae0948c38c91
Ajyma <ajymakockorova@gmail.com>
2022-07-23 00:37:21 +0600

    added skelet of register

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import Login from './components/Auth/Login';
+import Register from './components/Auth/Register'
 import {MainPages} from "./pages/General";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path={'/profile'} element={<MainPages.pages.Profile />} />
         <Route path={'/responses'} element={<MainPages.pages.Responses/>} />
         <Route path={'/auth/login'} element={<Login/>}/>
+        <Route path={'/auth/register'} element={<Register/>}/>
       </Routes>
     </div>
   );
--- src/components/Auth/Register/index.jsx
+++ src/components/Auth/Register/index.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import cls from './Register.module.scss'
+import { useNavigate } from 'react-router-dom'
 
-const Regoster = () => {
+const Register = () => {
+  const navigate = useNavigate()
+
   return (
-    <div>Regoster</div>
+    <div className={cls.register}>
+      <form>
+        <h1>Зарегистрироваться</h1>
+        <TextField
+          type='text'
+          placeholder={'Имя пользователя'}
+          name={'name'}
+        />
+
+        <TextField
+          type='email'
+          placeholder={'Электронная почта'}
+          name={'email'}
+        />
+
+        <TextField
+          type='password'
+          placeholder={'Пароль'}
+          name={'password'}
+        />
+
+        <div className={cls.btn}>
+          <Button
+            variant='contained'
+          >
+            Отправить
+          </Button>
+          <Button
+            variant='outlined'
+            onClick={() => navigate('/auth/login')}
+          >
+            Вход
+          </Button>
+        </div>
+      </form>
+    </div>
   )
 }
 
-export default Regoster
+export default Register
