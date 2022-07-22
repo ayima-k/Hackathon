b97dfb869f1c69b7e01c0053d76856c345ac45ea
altynovbayel <altynovich30@gmail.com>
2022-07-22 20:03:20 +0600

    bayel: routers

---
+++ src/pages/General/index.js
@@ -0,0 +1,15 @@
+import Main from './pages/Main'
+import Favorites from "./pages/Favorites/Favorites";
+import Profile from "./pages/Profile/Profile";
+import Responses from "./pages/Responses/Responses";
+
+const pages = {
+  Main,
+  Favorites,
+  Profile,
+  Responses,
+}
+
+export const MainPages = {
+  pages
+}
---
+++ src/pages/General/pages/Responses/Responses.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Responses(props) {
+  return (
+    <div>
+      Responses
+    </div>
+  );
+}
+
+export default Responses;
